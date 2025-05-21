36807ece8b95b52616256a47344cc41cfc390aac
rich12104 <rich.dc.818@gmail.com>
2025-05-21 15:37:16 -0400

    Update test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,18 +1,8 @@
 import sys
 import os
 
-<<<<<<< HEAD
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "/.."))
-
-import main  # Import your main.py functions
+import main 
 
 def test_greet():
     assert main.greet() == "Hello, GitHub Actions!"
-=======
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../workflow")))
 
-import main    # Import your main.py functions
-
-def test_greet():
-    assert main.greet() == "Hello, GitHub Actions!"
->>>>>>> 6ecd3aca41ba9afab709281a05ee0bdaf509a01e
